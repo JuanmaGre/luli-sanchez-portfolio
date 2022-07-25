8ed077fd2b6a1ad209867328a4a1e63d1d04d192
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-07-25 00:16:32 -0300

    centrado de landing

--- pages/index.js
+++ pages/index.js
@@ -12,38 +12,42 @@ export default function index() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             
-            <img 
-                src="/background.svg"
-                alt=""
-                // max-width={1920}
-                // max-height={1068}
-                className="absolute object-cover"
-            />
-            <div className='text-7xl tracking-wide content-center justify-center items-center font-title absolute ml-40'>
-                <h6 className='text-black tracking-tighter text-2xl ml-24 cursor-default'>
-                    Luli Sanchez
-                </h6>
-                <h1 className='text-[#6366F1]/50 cursor-default'>
-                    My Portfolio
-                </h1>
+            <div className='relative inline-block text-center'>
+                <img 
+                    src="/background.svg"
+                    alt=""
+                    className='bg-cover'
+                    // max-width={1920}
+                    // max-height={1068}
+                />
+                <div className='text-7xl tracking-wide content-center items-center
+            font-title'>
+                    <h6 className='text-black tracking-tighter text-2xl cursor-default'>
+                        Luli Sanchez
+                    </h6>
+                    <h1 className='text-[#6366F1]/50 cursor-default'>
+                        My Portfolio
+                    </h1>
+                </div>
+                <div>
+                    <Link href="/home">
+                        <button className='buttonLanding'>
+                            <a className="nav-link active" aria-current="page">
+                                english
+                            </a>        
+                        </button>
+                    </Link>
+                    
+                    <Link href="/homeSpa">
+                        <button className='buttonLanding'>
+                            <a className="nav-link active" aria-current="page">
+                                español
+                            </a>
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <div className='flex justify-center items-center py-40 space-x-40'>
-                <Link href="/home">
-                    <button className='buttonLanding'>
-                        <a className="nav-link active" aria-current="page">
-                            english
-                        </a>        
-                    </button>
-                </Link>
-                
-                <Link href="/homeSpa">
-                    <button className='buttonLanding'>
-                        <a className="nav-link active" aria-current="page">
-                            español
-                        </a>
-                    </button>
-                </Link>
-            </div>
+            
         </div>
-    )
+    );
 };
