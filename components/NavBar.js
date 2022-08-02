73c39862d09029726f9641f7b66c2ecb3d6ae924
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-08-02 12:35:27 -0300

    maquetado de servicios y corrección de navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,14 +6,14 @@ import { HomeIcon } from "@heroicons/react/outline"
 export default function Navbar() {
     return (
     <nav className="bg-[#ffedd5]/50 py-2.5">
-        <div className="flex justify-center items-center space-x-2 md:space-x-10">
-            <ul className="flex justify-self-start cursor-pointer hover:bg-[#a5b4fc] rounded-2xl">
+        <div className="flex justify-center items-center mr-4 space-x-2 md:space-x-10">
+            <ul className="flex -ml-12 items-center cursor-pointer hover:bg-[#a5b4fc] rounded-2xl">
                 <Link href="/home">
                     <HomeIcon 
                         className="nav-link active h-6 w-6"
                         aria-current="page"
                     />
-                </Link>    
+                </Link>
             </ul>
             <ul className="hidden space-x-4 md:flex">
                 <li className="navBarLinks">
--- pages/services.js
+++ pages/services.js
@@ -1,28 +1,56 @@
 import React from 'react'
+import Layout from '../components/Layout'
+
+
 
 function services() {
     return (
-        <div className='flex-row'>
-            <h1 className='flex justify-center my-7 text-6xl font-bold items-center'>
-                Services
-            </h1>
-            <h4 className='flex justify-center my-2 text-xl font-semibold items-center mb-40'>
-                Work with me
-            </h4>
+        <Layout>
+            <div className='flex-row'>
+                <h1 className='flex justify-center my-4 text-6xl font-title cursor-default'>
+                    Services
+                </h1>
+                <h4 className='flex justify-center my-6 text-xl font-semibold items-center mb-40 cursor-default'>
+                    Work with me
+                </h4>
 
-            <div className='flex justify-between'>
-                <div className='ml-20'>
-                    card 1
-                </div>
-                <div>
-                    card 2
-                </div>
-                <div className='mr-20'>
-                    card 3
+                <div className='flex justify-center -mt-32 px-4 mb-12 space-x-36'>
+                    <div>
+                        <img
+                            src="asd.jpeg"
+                            alt=""
+                            width={275}
+                            className='rounded-t-full'
+                        />
+                        <h1 className='justify-center text-center text-xl'>
+                            card 1
+                        </h1>
+                    </div>
+                    <div>
+                        <img
+                            src="asd.jpeg"
+                            alt=""
+                            width={275}
+                            className='rounded-t-full'
+                        />
+                        <h1 className='justify-center text-center'>
+                            card 2
+                        </h1>
+                    </div>
+                    <div>
+                        <img
+                            src="asd.jpeg"
+                            alt=""
+                            width={275}
+                            className='rounded-t-full'
+                        />
+                        <h1 className='justify-center text-center'>
+                            card 3
+                        </h1>
+                    </div>
                 </div>
             </div>
-        
-        </div>
+        </Layout>
     );
 };
 
